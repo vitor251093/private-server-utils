8296ee4fa9a37cf8680f9785a588d9ffd38dd129
VitorMM <vitor251093@gmail.com>
2022-05-06 22:58:55 -0300

    Improved sample of server mock

--- server-mock.js
+++ server-mock.js
@@ -1,4 +1,27 @@
+const getBody = function(req, callback) {
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk;
+    });
+    req.on('end', () => {
+        callback(body)
+    });
+}
+
 module.exports = function(req, res) {
+    if (req.url === "/avatar_mp_db/v2/gateway.php") {
+        getBody(req, function(body) {
+            if (body === "COMMAND=GET_VERSION") {
+                res.writeHead(200);
+                res.end("88");
+            }
+            else {
+                res.writeHead(404);
+                res.end("");
+            }
+        })
+        return
+    }
     res.writeHead(404);
     res.end("");
 }
